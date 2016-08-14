cc07d2e424ae60bbf3bd0e20b01cfe940589df91
Pavlo Deynega <paul.deynega@migonsoftware.com>
2016-08-14 16:10:09 +0300

    Minor updates.

--- SKTestApp/SKTestApp/AppConfig/AppDelegate.m
+++ SKTestApp/SKTestApp/AppConfig/AppDelegate.m
@@ -18,7 +18,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     [MagicalRecord setupCoreDataStack];
-    
+        
     return YES;
 }
 
