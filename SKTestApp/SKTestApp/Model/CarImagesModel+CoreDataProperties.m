8c993785604c671feab8cab7761e18831fe0046a
Pavlo Deynega <paul.deynega@migonsoftware.com>
2016-08-14 16:12:47 +0300

    Added images model.

---
+++ SKTestApp/SKTestApp/Model/CarImagesModel+CoreDataProperties.m
@@ -0,0 +1,19 @@
+//
+//  CarImagesModel+CoreDataProperties.m
+//  SKTestApp
+//
+//  Created by Pavlo Deynega on 10.08.16.
+//  Copyright © 2016 Pavlo Deynega. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+#import "CarImagesModel+CoreDataProperties.h"
+
+@implementation CarImagesModel (CoreDataProperties)
+
+@dynamic imagePath;
+@dynamic car;
+
+@end
---
+++ SKTestApp/SKTestApp/Model/CarImagesModel.m
@@ -0,0 +1,16 @@
+//
+//  CarImagesModel.m
+//  SKTestApp
+//
+//  Created by Pavlo Deynega on 09.08.16.
+//  Copyright © 2016 Pavlo Deynega. All rights reserved.
+//
+
+#import "CarImagesModel.h"
+#import "CarModel.h"
+
+@implementation CarImagesModel
+
+// Insert code here to add functionality to your managed object subclass
+
+@end
