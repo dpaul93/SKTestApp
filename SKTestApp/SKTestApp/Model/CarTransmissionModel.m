bfa94b0d15a74fdf3185ab074735a1e049b27617
Pavlo Deynega <paul.deynega@migonsoftware.com>
2016-08-14 16:13:03 +0300

    Added transmission model.

---
+++ SKTestApp/SKTestApp/Model/CarTransmissionModel.m
@@ -0,0 +1,16 @@
+//
+//  CarTransmissionModel.m
+//  SKTestApp
+//
+//  Created by Pavlo Deynega on 09.08.16.
+//  Copyright © 2016 Pavlo Deynega. All rights reserved.
+//
+
+#import "CarTransmissionModel.h"
+#import "CarModel.h"
+
+@implementation CarTransmissionModel
+
+// Insert code here to add functionality to your managed object subclass
+
+@end
