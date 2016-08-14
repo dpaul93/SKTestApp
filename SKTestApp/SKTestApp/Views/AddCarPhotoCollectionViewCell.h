e5425f157b2e0befb7bcef755bb1052d78881520
Pavlo Deynega <paul.deynega@migonsoftware.com>
2016-08-14 16:18:19 +0300

    Implemented collection view cell for add car view controller's collection view.

---
+++ SKTestApp/SKTestApp/Views/AddCarPhotoCollectionViewCell.h
@@ -0,0 +1,15 @@
+//
+//  AddCarPhotoCollectionViewCell.h
+//  SKTestApp
+//
+//  Created by Pavlo Deynega on 10.08.16.
+//  Copyright © 2016 Pavlo Deynega. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddCarPhotoCollectionViewCell : UICollectionViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *photoImageView;
+
+@end
---
+++ SKTestApp/SKTestApp/Views/AddCarPhotoCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  AddCarPhotoCollectionViewCell.m
+//  SKTestApp
+//
+//  Created by Pavlo Deynega on 10.08.16.
+//  Copyright © 2016 Pavlo Deynega. All rights reserved.
+//
+
+#import "AddCarPhotoCollectionViewCell.h"
+
+@implementation AddCarPhotoCollectionViewCell
+
+@end
